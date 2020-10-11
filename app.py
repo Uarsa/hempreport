6d0ebff2244bc15ece1dd0ecea689338825cd245
Uarsa <59147934+Uarsa@users.noreply.github.com>
2020-10-11 22:56:26 +0300

    working local version

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os, datetime, json
 from flask import Flask, flash, request, redirect, url_for, render_template, make_response
-from werkzeug.utils import secure_filename
 
 
 UPLOAD_FOLDER = "D:\hempreport\static\photos"
@@ -23,27 +22,29 @@ def upload():
         # description = request.form['description']
         date = datetime.datetime.now().strftime('%d/%m/%Y')
         info = request.form["info"]
-        photo = request.files["file"]
+        photo = request.files["photo"]
         photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photo.filename))
         # file.save(os.path.join('D:\\', 'files', 'photos'), file.filename)
-        
+        photo_name = photo.filename
+        # photo_name = "static\\photos" + "\\" + photo.filename   # WORKS!
+
         try:
-            file = open('today.json') 
+            file = open("today.json")
             report = json.load(file)
             counter = int(max(report))
-            report[counter+1] = [date, info, photo]
+            report[counter + 1] = [date, info, photo_name]
             file.close()
-        
+
         except FileNotFoundError:
             report = {}
-            file = open(filename, 'w')
-            report[1] = [date, info, photo]   # 0=date, 1=info, 2=photo
+            file = open("today.json", 'w')
+            report[1] = [date, info, photo_name]  # 0=date, 1=info, 2=photo
             file.close()
-    
-        file = open(filename, 'w')
+
+        file = open("today.json", 'w')
         json.dump(report, file)
         file.close()
-        
+
         return redirect('/view')
     else:
         return render_template('upload.html')
@@ -52,17 +53,20 @@ def upload():
 @app.route('/view')
 def view():
 
-    file = open('today.json')
+    file = open("today.json")
     report = json.load(file)
-    
-    sorted_report = sorted(report, reverse=True)
-    
-    file.close()
-    
+
+    sorted_keys_list = sorted(report, reverse=True)
+    sorted_report = {}
+
+    for k in sorted_keys_list:
+        sorted_report[k] = report[k]
+
     # return render_template('view.html', photo=photo_name, info=info, date=date)
     return render_template('view.html', report=sorted_report)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
+
